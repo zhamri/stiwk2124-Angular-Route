e4b3ba0116393d04bf5ac4c56a8e49eadedcdfaa
zhamri <zhamri@gmail.com>
2025-06-22 15:45:39 +0800

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {Course} from './course/course';
+import {About} from './about/about';
+import {Login} from './login/login';
+import {Home} from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'course',
+    component: Course,
+  },
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBar} from './nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
